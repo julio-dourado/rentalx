69fb49cfbdf03312aeef13bddf357a9274cde0fe
Júlio César <juliodourado419@gmail.com]>
2022-05-23 12:34:00 -0300

    validando o cadastro de categoria

--- src/routes/categoires.routes.ts
+++ src/routes/categoires.routes.ts
@@ -8,10 +8,21 @@ const CategoriesRepository1 = new CategoriesRepository()
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    CategoriesRepository1.create({name, description})
+    const categoryAlreadyExists = CategoriesRepository1.findByName(name);
 
+    if (categoryAlreadyExists) {
+        return response.status(400).json({error: "Category Already Exists"})
+    }
+
+    CategoriesRepository1.create({name, description});
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = CategoriesRepository1.list();
+
+    return response.json(all);
 });
 
 export { categoriesRoutes };
