2092875e829d6ab460908dd3a6b091520e5852ed
Júlio César <juliodourado419@gmail.com]>
2022-05-28 00:11:20 -0300

    refatorando as rotas

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,11 @@
 import { Router } from 'express';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationServer';
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request, response)=>{
-  const {name, description} = request.body;
-  const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-
-  createSpecificationService.execute({name, description});
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 })
 
 export { specificationsRoutes }
