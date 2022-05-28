022bbaecb1856e78c9dc267c476f61032fdbc340
Júlio César <juliodourado419@gmail.com]>
2022-05-27 22:51:48 -0300

    refatorando a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -0,0 +1,9 @@
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { ListCategoriesController } from "./lisCategoriesController";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+const categoriesRepository = new CategoriesRepository();
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
+
+export { listCategoriesController }
--- src/routes/categoires.routes.ts
+++ src/routes/categoires.routes.ts
@@ -1,20 +1,17 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-    const all = categoriesRepository.list();
-
-    return response.json(all);
+    return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
