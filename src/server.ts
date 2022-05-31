89a3e6cdbb0c692768f093a572caec615ad0b0db
Júlio César <juliodourado419@gmail.com]>
2022-05-31 15:40:40 -0300

    removendo os arquivos de upload

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import  SwaggerUi  from 'swagger-ui-express';
+
 import { router } from './routes';
+import swaggerFile from "./swagger.json";
+
 
 const app = express();
 
 app.use(express.json());
 
-app.use(router)
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
+
+app.use(router);
 
 app.listen(3333, () => console.log("Server is Running !"));
 
